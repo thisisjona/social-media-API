b372219e9c2b65a1e706787554d540ecac18c823
thisisjona <thisisjona@gmail.com>
2022-06-06 17:19:22 -0500

    added thoughtbyId routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const {
-    getAllThoughts
-    // getThoughtById,
-    // createThought
+    getAllThoughts,
+    getThoughtById,
+    createThought
 } = require('../../controllers/thought-controller');
 
 // /api/thoughts
-router.route('/').get(getAllThoughts);
+router.route('/').get(getAllThoughts).post(createThought);
+
+// /thoughts/<thoughtId>
+router.route(':thoughId').get(getThoughtById);
 
 module.exports = router;
